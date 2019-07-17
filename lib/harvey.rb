993d1fefdafe285ee55b352640f7e6dd872cf7ce
Joshua Adams <josh.adams@annkissam.com>
2019-07-17 15:24:10 -0400

    add rudimentary speech capabilities to `Harvey`

--- lib/harvey.rb
+++ lib/harvey.rb
@@ -1,6 +1,17 @@
 require "harvey/version"
 
+# Harvey is dumb. Harvey says things.
 module Harvey
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.harvey_speak(msg)
+    `espeak '#{msg}'`
+  end
+
+  def self.with_harvey(before: "", after: "", &block)
+    harvey_speak(before)
+    block.call if block_given?
+    harvey_speak(after)
+  end
 end
