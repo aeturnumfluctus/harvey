4353759633700832ae1a15fa9bca950a751dc671
Joshua Adams <josh.adams@annkissam.com>
2019-07-19 07:21:42 -0400

    better errors

--- lib/harvey/errors.rb
+++ lib/harvey/errors.rb
@@ -4,8 +4,9 @@ module Harvey
   class Error < StandardError; end
 
   class EspeakExecutionError < Error
-    def initialize(message, action)
-      super("HARVEY DON'T LIKE THAT! (Invocation of `espeak` failed)")
+    def initialize(msg)
+      msg ||= "HARVEY DON'T LIKE THAT! (Invocation of `espeak` failed)"
+      super(msg)
     end
   end
 end
